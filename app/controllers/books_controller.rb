f969835042a7c4b00de5c7b8596e60f722235756
EC2 Default User <ec2-user@ip-172-31-38-150.ec2.internal>
2022-06-15 14:47:58 +0000

    [Update]form_with厳格な書き方

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,15 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # 4. トップ画面へリダイレクト
+    redirect_to '/'
+  end
+
   def index
   end
 
@@ -11,4 +20,11 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+   private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
