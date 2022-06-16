be4306e19f10baf2ebff20c04ed8abcb3e9dd1dc
EC2 Default User <ec2-user@ip-172-31-38-150.ec2.internal>
2022-06-16 07:16:38 +0000

    [Update]バリデーション追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,11 +5,14 @@ class BooksController < ApplicationController
 
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
+    if @book.save
     # 4. トップ画面へリダイレクト
-    redirect_to book_path(book.id)
+      redirect_to book_path(book.id)
+    else
+      render :new
+    end
   end
 
   def index
@@ -25,9 +28,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -37,7 +43,7 @@ class BooksController < ApplicationController
   end
 
 
-   private
+  private
   # ストロングパラメータ
   def book_params
     params.require(:book).permit(:title, :body)
